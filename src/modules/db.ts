b27f7de41284358b5d476b1dd4cdeeb5243ec8bc
Anouar Kacem <kcm.anouar@gmail.com>
2022-10-15 14:11:48 +0100

    add previous function

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,3 +1,4 @@
+import { Context } from "../types/context";
 export interface Phase {
   id: number;
   title: string;
@@ -20,4 +21,28 @@ export interface Db {
 export const db: Db = {
   phases: [],
   tasks: [],
+};
+
+/**
+ * Find Task by ID
+ * @param ctx Context, graphql context
+ * @param id number, task id
+ * @returns Task
+ */
+export const findTask = (ctx: Context, id: number) => {
+  const task = ctx.db.tasks.find((t: Task) => t.id === id);
+  if (!task) throw "422 TASK NOT FOUND";
+  return task;
+};
+
+/**
+ *
+ * @param ctx context, graphql context
+ * @param id number, task id
+ * @returns Task
+ */
+export const previousTask = (ctx: Context, id: number) => {
+  if (id > 1) {
+    return findTask(ctx, id - 1);
+  }
 };
