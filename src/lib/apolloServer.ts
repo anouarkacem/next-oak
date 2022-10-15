6c9a62af904807a7ad68612548a86deec59daf82
Anouar Kacem <kcm.anouar@gmail.com>
2022-10-15 13:32:22 +0100

    fix import

--- src/lib/apolloServer.ts
+++ src/lib/apolloServer.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
 import { schema } from "@modules/graphql/schema";
-import { context } from "../types/context";
+import { context } from "../types/context.d";
 
 const server = new ApolloServer({
   schema,
