e9922a0b8210a9eebe5e93d2105ef10c7ec208ad
Anouar Kacem <kcm.anouar@gmail.com>
2022-10-15 13:02:22 +0100

    create nexus schema

---
+++ src/modules/graphql/schema.ts
@@ -0,0 +1,16 @@
+import { makeSchema } from "nexus";
+import { join } from "path";
+
+const schema = makeSchema({
+  types: [],
+  contextType: {
+    module: join(process.cwd(), "./src/types/context.d.ts"),
+    export: "Context",
+  },
+  outputs: {
+    schema: join(process.cwd(), "./generated/schema.graphql"),
+    typegen: join(process.cwd(), "./generated/nexus.typegen.d.ts"),
+  },
+});
+
+export { schema };
